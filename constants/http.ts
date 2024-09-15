6fcb6193b62e5d5dc0c0a924d31db64529f5d412
Abiola <ajibolaabiolaahmed@gmail.com>
2024-09-15 22:26:26 +0100

    feat: add constants

---
+++ constants/http.ts
@@ -0,0 +1,11 @@
+export const STATUS = {
+  FAIL: "fail",
+  SUCCESS: "success",
+  ERROR: "error",
+};
+
+export const MEESAGE = {
+    ERROR: "An Error occured, pease try again",
+    NOT_FOUND: "Not found",
+    INCORRECT_CREDS: "email or password incorrect"
+}
---
+++ constants/index.ts
@@ -0,0 +1,1 @@
+export {MEESAGE, STATUS} from "./http"
