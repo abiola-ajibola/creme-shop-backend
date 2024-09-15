3bed68dd286ce6ede52a49cd2fd9696f2dc05834
Abiola <ajibolaabiolaahmed@gmail.com>
2024-09-15 22:33:43 +0100

    chore: refactor products controller

--- controllers/productsControllers.ts
+++ controllers/productsControllers.ts
@@ -3,14 +3,16 @@ import { Product } from "../models";
 import { asyncHandler } from "../utils";
 
 export const getAllProducts = async (req: Request, res: Response) => {
-  const products = await Product.find();
+  const products = await Product.getAll();
   if (products) return res.json(products);
   res.status(500).json({ message: "Sorry, An error occured :(" });
 };
 
-export const getSingleProduct = asyncHandler(async (req: Request, res: Response) => {
-    const product = await Product.findById(req.params.productId);
+export const getSingleProduct = asyncHandler(
+  async (req: Request, res: Response) => {
+    const product = await Product.getById(req.params.productId);
     if (product) return res.json(product);
     res.status(404);
     throw new Error("The requested product was not found :(");
-  })
+  }
+);
